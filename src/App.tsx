f57229e0c592ad521051960abfed086000a81d18
LazySeaHorse <stevengrey.one@gmail.com>
2026-04-07 15:51:16 +0530

    Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
@@ -19,6 +20,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       <SpeedInsights />
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
